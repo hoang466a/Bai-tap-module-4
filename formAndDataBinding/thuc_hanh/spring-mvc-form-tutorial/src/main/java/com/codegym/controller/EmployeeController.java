5580d4e52807be69646784caf59136e14cdfcf15
hoang466a <hoangtn96@gmail.com>
2022-03-02 13:54:40 +0700

    day bai tap ngay 02/03/2022

--- formAndDataBinding/thuc_hanh/spring-mvc-form-tutorial/src/main/java/com/codegym/controller/EmployeeController.java
+++ formAndDataBinding/thuc_hanh/spring-mvc-form-tutorial/src/main/java/com/codegym/controller/EmployeeController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("employee/")
+@RequestMapping("/employee")
 public class EmployeeController {
 
-    @RequestMapping(value = "showForm", method = RequestMethod.GET)
+    @RequestMapping(value = "/showForm", method = RequestMethod.GET)
     public String showForm(ModelMap model) {
         model.addAttribute("employee", new Employee());
         return "employee/create";
