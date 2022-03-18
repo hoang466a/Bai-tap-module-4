8a772481c4f27c37669970490a89fd8f0df32df2
hoang466a <hoangtn96@gmail.com>
2022-03-18 15:38:44 +0700

    day bai tap ngay 18/03/2022

--- JPA/bai_tap/blog/src/main/java/com/example/test/controller/BlogController.java
+++ JPA/bai_tap/blog/src/main/java/com/example/test/controller/BlogController.java
@@ -10,11 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
+@RequestMapping({"/blog",""})
 public class BlogController {
     @Autowired
     BlogService blogService;
@@ -36,19 +38,22 @@ public class BlogController {
     public String save(Blog blog, RedirectAttributes redirectAttributes){
         blogService.save(blog);
         redirectAttributes.addFlashAttribute("message","Add new blog successfull");
-        return "/index";
+        return "redirect:/blog";
     }
 
-    @GetMapping("")
+    @GetMapping("/edit/{id}")
     public String edit(@PathVariable int id,Model model){
         model.addAttribute("blog",blogService.findById(id));
         return "/edit";
     }
 
-
-
-
-
+    @PostMapping("/update")
+    public String fix(Blog blog, Model model){
+        blogService.save(blog);
+        List<Blog>blogList= blogService.findAll();
+        model.addAttribute("blogList",blogList);
+        return "/index";
+    }
 
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable int id, Model model){
@@ -61,10 +66,10 @@ public class BlogController {
     public String delete_db(RedirectAttributes redirectAttributes,Blog blog){
         blogService.delete(blog);
         redirectAttributes.addFlashAttribute("message","Delete Blog succes!");
-        return "/index";
+        return "redirect:/blog";
     }
 
-    @GetMapping("/view")
+    @GetMapping("/view/{id}")
     public String view(@PathVariable int id,Model model){
         Blog blog=blogService.findOne(id);
         model.addAttribute("blog",blog);
