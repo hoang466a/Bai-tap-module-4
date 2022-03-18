4c4281972444e2679e8e5e3b173505b44101844b
hoang466a <hoangtn96@gmail.com>
2022-03-18 11:47:13 +0700

    day bai tap ngay 18/03/2022

--- JPA/bai_tap/blog/src/main/java/com/example/test/controller/BlogController.java
+++ JPA/bai_tap/blog/src/main/java/com/example/test/controller/BlogController.java
@@ -4,9 +4,11 @@ package com.example.test.controller;
 import com.example.test.model.Blog;
 import com.example.test.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.convert.ThreeTenBackPortConverters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,8 +36,41 @@ public class BlogController {
     public String save(Blog blog, RedirectAttributes redirectAttributes){
         blogService.save(blog);
         redirectAttributes.addFlashAttribute("message","Add new blog successfull");
-
         return "/index";
     }
+
+    @GetMapping("")
+    public String edit(@PathVariable int id,Model model){
+        model.addAttribute("blog",blogService.findById(id));
+        return "/edit";
+    }
+
+
+
+
+
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id, Model model){
+        Blog blog=blogService.findById(id);
+        model.addAttribute("blog",blog);
+        return "/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete_db(RedirectAttributes redirectAttributes,Blog blog){
+        blogService.delete(blog);
+        redirectAttributes.addFlashAttribute("message","Delete Blog succes!");
+        return "/index";
+    }
+
+    @GetMapping("/view")
+    public String view(@PathVariable int id,Model model){
+        Blog blog=blogService.findOne(id);
+        model.addAttribute("blog",blog);
+        return "/view";
+    }
+
+
 
 }
