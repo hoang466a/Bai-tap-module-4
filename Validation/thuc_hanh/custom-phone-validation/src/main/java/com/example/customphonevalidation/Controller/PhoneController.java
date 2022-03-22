f3e62068ad8af23756161f3460839977fc790e69
hoang466a <hoangtn96@gmail.com>
2022-03-22 17:17:38 +0700

    day bai tap ngay 22/03/2022

--- Validation/thuc_hanh/custom-phone-validation/src/main/java/com/example/customphonevalidation/Controller/PhoneController.java
+++ Validation/thuc_hanh/custom-phone-validation/src/main/java/com/example/customphonevalidation/Controller/PhoneController.java
@@ -1,4 +1,19 @@
 package com.example.customphonevalidation.Controller;
 
+
+import com.example.customphonevalidation.Model.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PhoneController {
+    @GetMapping("/")
+    public String showForm(Model model){
+        model.addAttribute("phoneNumber",new PhoneNumber);
+        return "/index";
+    }
 }
+
+
